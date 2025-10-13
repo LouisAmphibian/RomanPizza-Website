02792cb58739a81bc55d821350b458c2b9199f86
ayanda <ayandamantshinga30@gmail.com>
2025-10-13 16:12:06 +0200

    Initial commit: Romans Pizza with HTML, JavaScript and CSS

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example script for arrow navigation (you can expand this later)
+document.querySelectorAll('.arrow').forEach(arrow => {
+  arrow.addEventListener('click', () => {
+    alert('Promo navigation clicked!');
+  });
+});
